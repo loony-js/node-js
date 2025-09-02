7e0e2b955123f23dd196b97c097edecbc84ff117
Sankar Boro <sankar.boro@yahoo.com>
2025-09-02 08:52:17 +0530

    disable session on loony-api

--- packages/loony-api/src/init.ts
+++ packages/loony-api/src/init.ts
@@ -1,12 +1,11 @@
 import express from "express"
-import session from "express-session"
 import https from "https"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import fs from "fs"
 import config from "./config"
 
-const { SECRET_KEY, KEY_PATH, CERT_PATH } = config
+const { KEY_PATH, CERT_PATH } = config
 const options = {
   key: fs.readFileSync(KEY_PATH), // or localhost.key
   cert: fs.readFileSync(CERT_PATH), // or localhost.crt
@@ -20,14 +19,14 @@ app.use(
     credentials: true, // 🔥 allow cookies to be sent
   }),
 )
-app.use(
-  session({
-    secret: SECRET_KEY,
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false },
-  }),
-)
+// app.use(
+//   session({
+//     secret: SECRET_KEY,
+//     resave: false,
+//     saveUninitialized: false,
+//     cookie: { secure: false },
+//   }),
+// )
 app.use(cookieParser())
 app.use(express.json())
 const server = https.createServer(options, app)
